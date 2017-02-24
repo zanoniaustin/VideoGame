44dd27c0fe1aa62439c6b085f2e4328691cae224
Shawn Pacarar <spacarar@hotmail.com>
2017-02-24 14:32:52 -0700

    Added Media Manager & animation
    I added an animation based on the spritesheet. turned the spritesheet
    green. and fixed the media manager.

--- SuperHot.cpp
+++ SuperHot.cpp
@@ -8,22 +8,27 @@
 using namespace std;
 
 //media manager (needs work)
+//seems to be working
 class MediaManager {
 	map<string,SDL_Texture *> images;
 	//map<strin,SDL_MUSIC *> sounds;
 	public:
 	SDL_Texture *load(SDL_Renderer *ren, string imagePath){
-		if(images.find(imagePath.c_str())!=images.end()){
+		if(images.count(imagePath.c_str())==0){
+			cout << "loading image:  "<< imagePath<<endl;
 			SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
 			if(bmp!=NULL){
 				SDL_SetColorKey(bmp, SDL_TRUE, SDL_MapRGB(bmp->format, 0, 255, 0));
-				images[imagePath]=SDL_CreateTextureFromSurface(ren,bmp);	
+				images[imagePath]=SDL_CreateTextureFromSurface(ren,bmp);
+				
 			}
 			SDL_FreeSurface(bmp);
 		}
 		return images[imagePath];
 	}
+
 };
+
 
 class AnimationFrame {
 	SDL_Texture *frame; 
@@ -33,9 +38,14 @@ class AnimationFrame {
 	int srcW, srcH; //internal texture rectangle
 	public:
 	//Constructor to be used with Media Manager
-	AnimationFrame(SDL_Texture *newFrame, int newTime = 100) {
-		frame = newFrame;
+	AnimationFrame(SDL_Texture *newFrame, int newTime = 100,int srcx=0,int srcy=0,int srcW=24,int srcH=24) {
+		if(newFrame!=NULL)
+			frame = newFrame;
 		SDL_QueryTexture(newFrame,NULL,NULL, &w,&h);//get W & H
+		srcX=srcx;
+		srcY=srcy;
+		this->srcW=srcW;
+		this->srcH=srcH;
 		time = newTime;
 	}
 	//Constructor to be used for Manual Media
@@ -99,16 +109,15 @@ class Animation {
 	}
 
 	virtual void show(SDL_Renderer *ren, int time,int x=0,int y=0){
-		/*
 		int aTime = time % totalTime;
 		int tTime = 0;
 		unsigned int i;
 		for (i = 0; i <frames.size(); i++){
 			tTime += frames[i]->getTime();
 			if (aTime <= tTime)break;
-		}*/
+		}
 		//renderer , posX, posY, offset on texture, width on texture
-		frames[0]->show(ren,x,y);
+		frames[i]->show(ren,x,y);
 	}	
 	virtual void destroy() {
 		for (unsigned int i = 0; i < frames.size(); i++)
@@ -192,6 +201,8 @@ class Game{
 		SDL_Quit();
 	}
 	void run() {
+		int start = SDL_GetTicks();
+		int oldTicks = start;
 		finished = false;
 		while(!finished) {
 			SDL_Event event;
@@ -206,10 +217,15 @@ class Game{
 				}
 				if(!finished) handleEvent(event);
 			}
+			ticks = SDL_GetTicks();
+			dt = (float)(ticks-oldTicks)/1000.0;
+			oldTicks = ticks;
 			SDL_RenderClear(ren);
 			show();
 			SDL_RenderPresent(ren);
 		}
+		int end = SDL_GetTicks();
+		cout << "FPS " << (300.0*1000.0/float(end-start)) <<endl;
 	}	
 	virtual void show() = 0;
 	virtual void handleEvent(SDL_Event &event) = 0;
@@ -223,7 +239,11 @@ class myGame:public Game {
 	public:
 	void init(const char *gameName = "My Game", int maxW=640, int maxH=480, int startX=100, int startY=100) {
 		Game::init(gameName);
-		shoot.addFrame(new AnimationFrame(ren,"CharacterSprite.bmp",200,156,252,24,24));
+		//BUILD SPRITE WITH (ren,time,internal X, internal y, width, height)
+		//use texHandle to save FPS
+		shoot.addFrame(new AnimationFrame(texHandle.load(ren,"CharacterSprite.bmp"),300,158,252,30,24));
+		shoot.addFrame(new AnimationFrame(texHandle.load(ren,"CharacterSprite.bmp"),50,216,255,30,24));
+		shoot.addFrame(new AnimationFrame(texHandle.load(ren,"CharacterSprite.bmp"),50,245,256,30,24));
 	}
 	void show(){
 		shoot.show(ren,ticks);

dd9bb6f41307963cd2896d6da644ab4002cb07c3
Shawn Pacarar <spacarar@hotmail.com>
2017-03-10 10:47:44 -0700

    Fixed broken code
    When I was merging my multithreaded code with your enemy class i
    accidentally broke some stuff, fixed it back.

--- SuperHot.cpp
+++ SuperHot.cpp
@@ -282,6 +282,48 @@ class Player:public Sprite{
 	}
 };
 
+class Enemy:public Sprite{
+	int frameID;
+	public:
+	Enemy(){
+		x=200;
+		y=200;
+		dx=0;
+		dy=0;
+	}
+	Enemy(float x, float y, float dx, float dy){
+		this->x=x;
+		this->y=y;
+		this->dx=dx;
+		this->dy=dy;
+	}
+	void loadEnemy(SDL_Renderer *ren,MediaManager &texHandle){
+		frameID = 0;
+		SDL_Rect frameRect; //used to create sprite frames (x,y,w,h)
+		setRect(frameRect,158,252,30,24);
+		this->addFrame(new AnimationFrame(texHandle.load(ren,"CharacterSprite.bmp"),frameRect,300)); //media manager handle
+		/*
+		setRect(frameRect,216,255,30,24);
+		this->addFrame(new AnimationFrame(texHandle.load(ren,"CharacterSprite.bmp"),frameRect,50));
+		
+		setRect(frameRect,245,256,30,24);
+		this->addFrame(new AnimationFrame(texHandle.load(ren,"CharacterSprite.bmp"),frameRect,50));*/
+	}
+	void showFrame(SDL_Renderer *ren, SDL_Rect &camera,int time){
+		frames[frameID]->show(ren,camera,x,y);
+	}
+	void setRect(SDL_Rect &rect,int x,int y, int w, int h){
+		rect.x=x;
+		rect.y=y;
+		rect.w=w;
+		rect.h=h;
+	}
+	void destroy(){
+		Sprite::destroy();
+
+	}
+};
+
 
 class Game{
 	SDL_Thread *updateThread,*renderThread;

0a7503f4cdc004f284190de8a646296dbe77815c
Tyler Gillson <tyler.gillson@gmail.com>
2017-04-13 15:57:09 -0600

    fixed undeclared var bug

--- src/Player.hpp
+++ src/Player.hpp
@@ -25,6 +25,7 @@ class Player:public Sprite{
 		center->x=x;
 		center->y=y;
 		angle = 0;
+		frameID = 0;
 	}
 	Player(float x, float y, float dx, float dy){
 		this->x=x;
@@ -32,43 +33,43 @@ class Player:public Sprite{
 		this->dx=dx;
 		this->dy=dy;
 	}
-	
+
 	void loadPlayer(SDL_Renderer *ren,MediaManager &texHandle){
 		SDL_Rect frameRect;
 		setRect(frameRect,158,252,30,24);
 		this->addFrame(new AnimationFrame(texHandle.load(ren,"../assets/CharacterSprite.bmp"),frameRect,300)); //media manager handle
-		
+
 		setRect(frameRect,216,255,30,24);
 		this->addFrame(new AnimationFrame(texHandle.load(ren,"../assets/CharacterSprite.bmp"),frameRect,50));
-		
+
 		setRect(frameRect,245,256,30,24);
 		this->addFrame(new AnimationFrame(texHandle.load(ren,"../assets/CharacterSprite.bmp"),frameRect,50));
 	}
-	
+
 	void showFrame(SDL_Renderer *ren, SDL_Rect &camera,int time){
 		frames[frameID]->show(ren,camera,center,angle,x,y);
 		frameID = 0;
 	}
-	
+
 	void setRect(SDL_Rect &rect,int x,int y, int w, int h){
 		rect.x=x;
 		rect.y=y;
 		rect.w=w;
 		rect.h=h;
 	}
-	
+
 	virtual void update(){
 		center->x=x;
 		center->y=y;
 		Sprite::update();
 	}
-	
+
 	void destroy(){
 		Sprite::destroy();
 	}
 	//sprite can handle his own event
 	void handleMyEvent(SDL_Event &e){
-	
+
 	}
 };
 
@@ -101,7 +102,7 @@ class Enemy:public Sprite{
 		/*
 		setRect(frameRect,216,255,30,24);
 		this->addFrame(new AnimationFrame(texHandle.load(ren,"CharacterSprite.bmp"),frameRect,50));
-		
+
 		setRect(frameRect,245,256,30,24);
 		this->addFrame(new AnimationFrame(texHandle.load(ren,"CharacterSprite.bmp"),frameRect,50));*/
 	}
@@ -114,7 +115,7 @@ class Enemy:public Sprite{
 		if(px == x) dx=0;
 		else if(px<x) dx=-1;
 		else if(px>x) dx=1;
-		
+
 		if(py==y)dy=0;
 		else if(py<y)dy=-1;
 		else if(py>y)dy=1;
